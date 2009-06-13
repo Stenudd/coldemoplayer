c7736f33179004b1fdc0f77cea0572ceefac803c
young.jpc <young.jpc@57aa2718-e68a-11dd-a6ea-a32d6b26a33d>
2009-06-13 10:15:02 +0000

    Tagged 1.1.7.

---
+++ tags/1.1.7/compLexity Demo Player/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.IO;
+
+namespace compLexity_Demo_Player
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Common.LogException(e.Exception);
+            Common.Message(null, "Unhandled exception.", e.Exception, MessageWindow.Flags.Error);
+        }
+    }
+}
